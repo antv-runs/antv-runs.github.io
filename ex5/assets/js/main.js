5d6b681a8b2ddde2f09e9e32b73ac7df694000d0
antv-runs <antv@runsystem.net>
2026-02-05 15:28:20 +0700

    ex5: init

---
+++ ex5/assets/js/main.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    name: "A",
+    value: 2,
+  },
+  {
+    name: "B",
+    value: 0.1,
+  },
+  {
+    name: "C",
+    value: 3,
+  },
+  {
+    name: "E",
+    value: 4,
+  },
+  {
+    name: "F",
+    value: 4,
+  },
+];
