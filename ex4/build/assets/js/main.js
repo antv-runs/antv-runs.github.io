c2895ce3c3a2b93130f2b60a9728a572ab383ae0
antv-runs <antv@runsystem.net>
2026-02-05 08:58:55 +0700

    ex4: finish filtering by stars

--- ex4/build/assets/js/main.js
+++ ex4/build/assets/js/main.js
@@ -3,7 +3,7 @@ const productTabContent = document.querySelectorAll(".products-tabs__content");
 const filterByStarsBtn = document.getElementById("btn-filter-by-stars");
 const filterDropdown = document.getElementById("dropdown-filter-by-stars");
 const filterOptions = document.querySelectorAll(".reviews__filter-option");
-const reviews = document.querySelectorAll("reviews__item");
+const reviews = document.querySelectorAll(".reviews__item");
 
 productTabs.forEach((tab) => {
   tab.addEventListener("click", () => {
@@ -26,4 +26,33 @@ filterByStarsBtn.addEventListener("click", (e) => {
   e.stopPropagation();
   console.log("click on filter by stars button");
   filterDropdown.classList.toggle("reviews__filter-dropdown--show");
+});
+
+filterOptions.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    e.stopPropagation();
+    console.log("Click on the filter option");
+    filterOptions.forEach((opt) => {
+      opt.classList.remove("reviews__filter-option--active");
+    });
+
+    option.classList.add("reviews__filter-option--active");
+    const selectedOption = option.dataset.option;
+    reviews.forEach((review) => {
+      const reviewStars = review.dataset.stars;
+      if (selectedOption === "All" || reviewStars === selectedOption) {
+        review.style.display = "block";
+      } else {
+        review.style.display = "none";
+      }
+    });
+  });
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    !filterDropdown.contains(e.target) &&
+    !filterByStarsBtn.contains(e.target)
+  )
+    filterDropdown.classList.remove("reviews__filter-dropdown--show");
 });
